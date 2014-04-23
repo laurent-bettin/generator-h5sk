02e85096aa3b58332f32d558bba6ff35fe0ccb8d
laurent-bettin <b.rhaze@gmail.com>
2014-04-23 19:59:46 +0200

    Usererf task

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,6 +1,16 @@
 'use strict';
 var gulp = require('gulp');
+var useref = require('gulp-useref');
+
+gulp.task('useref', function() {
+    return gulp.src('app/*.html')
+        .pipe(useref.assets())
+        .pipe(useref.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('default', function() {
 
+    gulp.start('useref');
 });
